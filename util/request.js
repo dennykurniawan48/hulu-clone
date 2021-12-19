896330bd7012247cf3b97dd0d1bebb4af102ba02
dennykurniawan48 <kurniawan.denny.48.com>
2021-12-20 01:56:58 +0700

    fix endpoint

--- util/request.js
+++ util/request.js
@@ -10,38 +10,38 @@ export default{
     },
     fetchAction:{
         title: "Action",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=28`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
     },
     fetchComedy:{
         title: "Comedy",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=35`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`
     },
     fetchHorror:{
         title: "Horror",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=27`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`
     },
     fetchRomance:{
         title: "Romance",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=10749`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`
     },
     fetchMystery:{
         title: "Mystery",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=9648`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=9648`
     },
     fetchScifi:{
         title: "Sci-fi",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=878`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=878`
     },
     fetchWestern:{
         title: "Western",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=37`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=37`
     },
     fetchAnimation:{
         title: "Animation",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=16`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=16`
     },
     fetchTv:{
         title: "Tv Movie",
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genre=10770`
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10770`
     }
 }
